bd488550a777fc5b46a826e9657d430a9fdc784d
saloni <saloni.mourya@axeno.co>
2026-09-13 23:51:05 +0530

    url update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,7 @@ const locationRoutes = require("./routes/locationRoutes");
 const app = express();
 
 const corsOptions = {
-  origin: 'https://shopco-frontend-o66mt9ayc-saloni-c9e6.vercel.app/',
+  origin: 'https://shopco-frontend-o66mt9ayc-saloni-c9e6.vercel.app',
   optionsSuccessStatus: 200,
    credentials: true,
 };
